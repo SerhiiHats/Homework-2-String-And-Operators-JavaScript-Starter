094995b7845368dfeb4fffeb22ef094a1689eee2
Serhi Hats <gatsserv@gmail.com>
2023-01-26 02:13:41 +0200

    Add first commit in homework2 at JavaScript starter

---
+++ task4/simple.js
@@ -0,0 +1,9 @@
+let firstNumber = 10;
+let secondaryNumber = 20;
+alert(`${firstNumber} > ${secondaryNumber} = ${firstNumber>secondaryNumber}
+${firstNumber} < ${secondaryNumber} = ${firstNumber<secondaryNumber}
+${firstNumber} >= ${secondaryNumber} = ${firstNumber>=secondaryNumber}
+${firstNumber} <= ${secondaryNumber} = ${firstNumber<=secondaryNumber}
+${firstNumber} == ${secondaryNumber} = ${firstNumber==secondaryNumber}
+${firstNumber} != ${secondaryNumber} = ${firstNumber!=secondaryNumber}
+${firstNumber} === ${secondaryNumber} = ${firstNumber===secondaryNumber}`);
